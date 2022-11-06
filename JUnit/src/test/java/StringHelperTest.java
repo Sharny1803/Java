bcb22781ed8be0829af14a3cd91d85a72b8c3421
Sharny1803 <102305477+Sharny1803@users.noreply.github.com>
2022-11-07 00:18:30 +0100

    Add files via upload

---
+++ JUnit/src/test/java/StringHelperTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class StringHelperTest{
+
+    @Test
+    public void test(){
+        StringHelper helper = new StringHelper();
+        assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+    }
+}
+
+
